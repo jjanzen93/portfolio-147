b245ecc95cbabb0a81c47b19688fcc7e61685ab9
jjanzen93 <132104176+jjanzen93@users.noreply.github.com>
2025-04-10 21:02:34 -0700

    adding experiment 1 from glitch

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -7,28 +7,40 @@
 // Constants - User-servicable parts
 // In a longer project I like to put these in a separate file
 
-// define a class
-class MyProjectClass {
-  // constructor function
-  constructor(param1, param2) {
-    // set properties using 'this' keyword
-    this.property1 = param1;
-    this.property2 = param2;
-  }
+const fillers = {
+  addendum: ["but it's", "so i'm", "and it's", "and it isn't", "but it isn't", "so it's", "so i'm not", "so it isn't", "and it's not", "but it's not", "so it's not"],
+  adjective: ["good", "bad", "better", "worse", "red", "yellow", "green", "purple", "brown", "black", "white", "salty", "sweet", "big", "bigger", "smaller", "small", "average", "normal", "extraordinary", "terrible", "awful", "a significant downgrade", "happy", "sad", "evil", "kind", "in a new era of economic progress", "somewhat akin to a twenty one pilots song"],
+  noun: ["bread", "brat", "quantum physics", "the united states government", "the planet mars", "water", "humanity", "a tortilla", "a bird", "a pet rock", "dwayne 'the rock' johnson", "the kitchen sink", "a blender", "a cat", "a dog", "the best album ever", "the worst thing ever", "the best twenty one pilots song"]
   
-  // define a method
-  myMethod() {
-    // code to run when method is called
+};
+
+const template = `$noun, $addendum $adjective $addendum $adjective $addendum $adjective`;
+
+
+// STUDENTS: You don't need to edit code below this line.
+
+const slotPattern = /\$(\w+)/;
+
+function replacer(match, name) {
+  let options = fillers[name];
+  if (options) {
+    return options[Math.floor(Math.random() * options.length)];
+  } else {
+    return `<UNKNOWN:${name}>`;
   }
 }
 
-function main() {
-  // create an instance of the class
-  let myInstance = new MyProjectClass("value1", "value2");
+function generate() {
+  let story = template;
+  while (story.match(slotPattern)) {
+    story = story.replace(slotPattern, replacer);
+  }
 
-  // call a method on the instance
-  myInstance.myMethod();
+  /* global box */
+  $("#box").text(story);
 }
 
-// let's get this party started - uncomment me
-//main();
+/* global clicker */
+$("#clicker").click(generate);
+
+generate();
