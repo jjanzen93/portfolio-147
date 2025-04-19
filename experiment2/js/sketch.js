9176e485044479d556980ffaf57195d87df3ec22
jjanzen93 <132104176+jjanzen93@users.noreply.github.com>
2025-04-18 17:45:17 -0700

    added exp 2 code from glitch

--- experiment2/js/sketch.js
+++ experiment2/js/sketch.js
@@ -13,6 +13,7 @@ const VALUE2 = 2;
 // Globals
 let myInstance;
 let canvasContainer;
+let seed = 0;
 var centerHorz, centerVert;
 
 class MyClass {
@@ -51,6 +52,29 @@ function setup() {
   resizeScreen();
 }
 
+// star function taken from p5.js examples
+function star(x, y, radius1, radius2, npoints) {
+  let angle = TWO_PI / npoints;
+  let halfAngle = angle / 2.0;
+  beginShape();
+  for (let a = 0; a < TWO_PI; a += angle) {
+    let sx = x + cos(a) * radius2;
+    let sy = y + sin(a) * radius2;
+    vertex(sx, sy);
+    sx = x + cos(a + halfAngle) * radius1;
+    sy = y + sin(a + halfAngle) * radius1;
+    vertex(sx, sy);
+  }
+  endShape(CLOSE);
+}
+
+// shift shape size with mouse position
+function getScale(x, y) {
+  let d = dist(x, y, mouseX, mouseY);
+  let maxDist = dist(0, 0, width, height);
+  return map(d, 150, maxDist, 1, 0);
+}
+
 // draw() function is called repeatedly, it's the main animation loop
 function draw() {
   background(220);    
@@ -65,12 +89,68 @@ function draw() {
   noStroke();
   rect(-125, -125, 250, 250); // Draw the rectangle centered on the new origin
   pop(); // Restore the original drawing context
+  randomSeed(seed);
+  const shapes = 1000*random()
+
+  // triangles
+  let color = Math.floor(random() * availableColors.length)
+  stroke(availableColors[color]);
+  for (let i = 0; i < shps; i++) {
+    let x = width * random();
+    let y = height * random();
+    rotate(360*random());
+    let scale = getScale(x, y);
+    triangle(x, y , x - 35*scale, y, x, y - 20*scale);
+  }
+  
+  // squares
+  color++;
+  if (color == availableColors.length) {
+    color = 0;
+  }
+  stroke(availableColors[color]);
+  for (let i = 0; i < shps; i++) {
+    let x = width * random();
+    let y = height * random();
+    rotate(360*random());
+    let scale = getScale(x, y);
+    square(x, y, 25*scale);
+  }
+  
+  // stars
+  color++;
+  if (color == availableColors.length) {
+    color = 0;
+  }
+  stroke(availableColors[color]);
+  for (let i = 0; i < shps; i++) {
+    let x = width * random();
+    let y = height * random();
+    rotate(360*random());
+    let scale = getScale(x, y);
+    star(x, y, 5*scale, 12*scale, 5);
+  }
+  
+  // circles
+  color++;
+  if (color == availableColors.length) {
+    color = 0;
+  }
+  stroke(availableColors[color]);
+  for (let i = 0; i < shps; i++) {
+    let x = width * random();
+    let y = height * random();
+    rotate(360*random());
+    let scale = getScale(x, y);
+    circle(x, y, 25*scale);
+  }
 
   // The text is not affected by the translate and rotate
   fill(255);
   textStyle(BOLD);
   textSize(140);
   text("p5*", centerHorz - 105, centerVert + 40);
+
 }
 
 // mousePressed() function is called once after every time a mouse button is pressed
